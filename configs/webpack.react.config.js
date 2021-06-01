c12c068d27aec6e70f0c0f9fe936fa046365ae75
YF1999 <1586939317@qq.com>
2021-06-02 02:09:51 +0800

    chore(configs): Add webpack configs for react projects

--- configs/webpack.react.config.js
+++ configs/webpack.react.config.js
@@ -7,11 +7,29 @@
 // - webpack.config.js:
 //   https://github.com/facebook/create-react-app/blob/v4.0.3/packages/react-scripts/config/webpack.config.js
 
+const _path = require('path');
+
+// Edited from `https://github.com/facebook/create-react-app/blob/v4.0.3/packages/react-scripts/config/paths.js`
+// Line 17-18
+const _projectDir = _path.resolve(__dirname, '..');
+const ResolveProject = (relativePath) => _path.resolve(_projectDir, relativePath);
+
+// Edited from `https://github.com/facebook/create-react-app/blob/v4.0.3/packages/react-scripts/config/paths.js`
+// Line 62-79
+const _paths = {
+    appBuild: ResolveProject('build'),
+    appHtml: ResolveProject('public/index.html'),
+    appIndexTs: ResolveProject('src/index.tsx'),
+    appPublic: ResolveProject('public'),
+    appSrc: ResolveProject('src'),
+    appTsConfig: ResolveProject('tsconfig.json'),
+    proxySetup: ResolveProject('src/setupProxy.js'),
+    testsSetup: ResolveProject('src/setupTests.ts'),
+};
+
 // const fs = require('fs');
 
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
-
-const BaseWebpack = require('./webpack.base.config');
 
 // Write configurations as json data into file, for debugging.
 // function WriteConfigs(config, filename) {
@@ -31,11 +49,7 @@ function OverrideWebpackConfigs(webpack, env) {
     // 2. Set the correct directories where the source code should be compiled.
 
     // WriteConfigs(webpack.module.rules, 'webpack.json');
-    webpack.module.rules[1].oneOf[2].include = [BaseWebpack.paths.appSrcRenderer, BaseWebpack.paths.appSrcShared];
-
-    // 3. Use custom aliases.
-
-    webpack.resolve.alias = BaseWebpack.alias;
+    // webpack.module.rules[1].oneOf[2].include = [_paths.appSrc];
 
     // Finish.
 
@@ -44,13 +58,13 @@ function OverrideWebpackConfigs(webpack, env) {
 
 // Override paths configurations.
 function OverridePathsConfigs(paths, env) {
-    paths.appPublic = BaseWebpack.paths.appPublic;
-    paths.appHtml = BaseWebpack.paths.appHtml;
-    paths.appIndexJs = BaseWebpack.paths.appIndexTsRenderer;
-    paths.appSrc = BaseWebpack.paths.appSrc;
-    paths.appTsConfig = BaseWebpack.paths.appTsConfigRenderer;
-    paths.testsSetup = BaseWebpack.paths.testsSetup;
-    paths.proxySetup = BaseWebpack.paths.proxySetup;
+    paths.appPublic = _paths.appPublic;
+    paths.appHtml = _paths.appHtml;
+    paths.appIndexJs = _paths.appIndexTs;
+    paths.appSrc = _paths.appSrc;
+    paths.appTsConfig = _paths.appTsConfig;
+    paths.testsSetup = _paths.testsSetup;
+    paths.proxySetup = _paths.proxySetup;
 
     // WriteConfigs(paths, 'paths.json');
 
