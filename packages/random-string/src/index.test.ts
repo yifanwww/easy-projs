af3051c879d78e349ac60e060950f76123ee99b4
yifanwww <yifanw1101@gmail.com>
2021-12-21 00:16:40 +0800

    fix: Fix tests

--- packages/random-string/src/index.test.ts
+++ packages/random-string/src/index.test.ts
@@ -1,3 +1,5 @@
+import { expectSnapshot } from '@easy/utils-test';
+
 import * as package_exports from './index';
 
 it('exports from this package', () => expectSnapshot(Object.keys(package_exports)));
