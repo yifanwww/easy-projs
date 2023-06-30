31401b3e4a7f3261b634e009bb8ab36bb2a83e75
yifanwww <yifanw1101@gmail.com>
2023-07-01 02:21:45 +0800

    feat: add assert helpers

--- packages/utils/src/assert.ts
+++ packages/utils/src/assert.ts
@@ -1,3 +1,12 @@
+/**
+ * @param value The input that is checked for being truthy.
+ */
+export function assert(value: unknown): asserts value {
+    if (!value) {
+        throw new Error('Assertion Error');
+    }
+}
+
 export function assertIsString(value: unknown, varName: string): asserts value is string {
     if (typeof value !== 'string') {
         throw new Error(`Assertion Error: ${varName} should be string`);
