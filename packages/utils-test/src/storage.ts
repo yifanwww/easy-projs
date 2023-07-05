d8889e43bdc4770009735f8fe4c5b72262ee4331
yifanwww <yifanw1101@gmail.com>
2023-07-05 13:50:18 +0800

    feat: add browser storage helpers

--- packages/utils-test/src/storage.ts
+++ packages/utils-test/src/storage.ts
@@ -1,39 +1,36 @@
 /**
  * Mock implementation of local storage.
  */
-export function mockLocalStorage(): void {
+export function mockStorage(storage: Storage): void {
     // Mocks will be restored before every test because Jest option `resetMocks` is turned on.
     beforeEach(() => {
         let store: Record<string, string> = {};
 
-        const prototype = Reflect.getPrototypeOf(window.localStorage) as typeof window.localStorage;
+        const prototype = Reflect.getPrototypeOf(storage) as Storage;
 
-        jest.spyOn(prototype, 'getItem').mockImplementation((key) => (key in store ? store[key] : null));
-        jest.spyOn(prototype, 'setItem').mockImplementation((key, value) => {
-            store[key] = value;
-        });
+        // properties
+
+        jest.spyOn(prototype, 'length', 'get').mockImplementation(() => Object.keys(store).length);
+
+        // methods
+
         jest.spyOn(prototype, 'clear').mockImplementation(() => {
             store = {};
         });
-    });
-}
-
-/**
- * Mock implementation of session storage.
- */
-export function mockSessionStorage(): void {
-    // Mocks will be restored before every test because Jest option `resetMocks` is turned on.
-    beforeEach(() => {
-        let store: Record<string, string> = {};
-
-        const prototype = Reflect.getPrototypeOf(window.sessionStorage) as typeof window.sessionStorage;
 
         jest.spyOn(prototype, 'getItem').mockImplementation((key) => (key in store ? store[key] : null));
+
+        jest.spyOn(prototype, 'key').mockImplementation((index) => {
+            const keys = Object.keys(store);
+            return index >= keys.length ? null : keys[index];
+        });
+
+        jest.spyOn(prototype, 'removeItem').mockImplementation((key) => {
+            delete store[key];
+        });
+
         jest.spyOn(prototype, 'setItem').mockImplementation((key, value) => {
             store[key] = value;
-        });
-        jest.spyOn(prototype, 'clear').mockImplementation(() => {
-            store = {};
         });
     });
 }
