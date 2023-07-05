11c0dfbe1195a62b181fc777980410eb726cf77a
yifanwww <yifanw1101@gmail.com>
2023-07-05 13:41:00 +0800

    chore(deps): refresh deps

--- packages/utils-antd/src/__tests__/select.test.ts
+++ packages/utils-antd/src/__tests__/select.test.ts
@@ -6,16 +6,15 @@ describe(`Test static method \`${SelectUtil.name}.${SelectUtil.mergeDefaultField
     it('should merge default field names', () => {
         const expected: FieldNames = {
             label: 'label',
-            options: 'options',
             value: 'value',
+            options: 'options',
+            groupLabel: 'label',
         };
 
         expect(SelectUtil.mergeDefaultFieldNames()).toStrictEqual(expected);
         expect(SelectUtil.mergeDefaultFieldNames({})).toStrictEqual(expected);
-        expect(SelectUtil.mergeDefaultFieldNames({ label: 'l', value: 'v', options: 'o' })).toStrictEqual({
-            label: 'l',
-            value: 'v',
-            options: 'o',
-        });
+        expect(
+            SelectUtil.mergeDefaultFieldNames({ label: 'l', value: 'v', options: 'o', groupLabel: 'l' }),
+        ).toStrictEqual({ label: 'l', value: 'v', options: 'o', groupLabel: 'l' });
     });
 });
