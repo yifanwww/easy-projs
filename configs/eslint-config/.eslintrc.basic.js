6fba5b68a04e2d51ee32e494d5aa3b1f855c5db2
yifanwww <yifanw1101@gmail.com>
2023-07-08 22:06:24 +0800

    chore(eslint): don't apply react relevant rules to non-react packages

--- configs/eslint-config/.eslintrc.basic.js
+++ configs/eslint-config/.eslintrc.basic.js
@@ -46,7 +46,6 @@ const naming = [
 ];
 
 module.exports = {
-    root: true,
     env: {
         browser: true,
         es2017: true,
@@ -54,8 +53,7 @@ module.exports = {
         node: true,
     },
     extends: [
-        'airbnb',
-        'airbnb/hooks',
+        'airbnb-base',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         // TODO: may enable this configuration set in the future
@@ -64,7 +62,7 @@ module.exports = {
         'prettier/prettier',
     ],
     ignorePatterns: ['*.cjs', '*.js', '*.mjs'],
-    plugins: ['@typescript-eslint', 'import', 'jest', 'jsx-a11y', 'node', 'prettier', 'react', 'react-hooks'],
+    plugins: ['@typescript-eslint', 'import', 'jest', 'node', 'prettier'],
 
     settings: {
         // Append 'ts' extensions to Airbnb 'import/extensions' setting
@@ -344,44 +342,6 @@ module.exports = {
 
         // Enables prettier rules.
         'prettier/prettier': 'error',
-
-        // -------------------- Eslint-Plugin-React Rules --------------------
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-        'react/destructuring-assignment': 'off',
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
-        'react/function-component-definition': 'off',
-
-        // Append 'tsx' to Airbnb 'react/jsx-filename-extension' rule
-        // Original: ['.jsx']
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-useless-fragment.md
-        'react/jsx-no-useless-fragment': 'off',
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-one-expression-per-line.md
-        'react/jsx-one-expression-per-line': 'off',
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
-        'react/jsx-props-no-spreading': 'off',
-
-        // Disable because we only use TypeScript to write components, no need to use `prop-types`
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-        'react/prop-types': 'off',
-
-        // Disable this rule for using new JSX transform from React 17.
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
-        'react/react-in-jsx-scope': 'off',
-
-        // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
-        'react/require-default-props': 'off',
-
-        // -------------------- Eslint-Plugin-React-Hooks Rules --------------------
-
-        // https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
-        'react-hooks/exhaustive-deps': ['error', { additionalHooks: 'use[a-zA-Z]+Effect' }],
     },
     overrides: [
         {
