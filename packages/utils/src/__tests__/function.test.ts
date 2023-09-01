e1f3ef08f09e7b36c7dba8c1ad09ff41fe79125e
yifanwww <yifanw1101@gmail.com>
2023-09-01 20:29:11 +0800

    feat: add `noAsync` util function

--- packages/utils/src/__tests__/function.test.ts
+++ packages/utils/src/__tests__/function.test.ts
@@ -1,4 +1,4 @@
-import { abstractAsyncFn, abstractFn } from '../function.js';
+import { abstractAsyncFn, abstractFn, noAsync } from '../function.js';
 
 describe(`Test fn ${abstractFn.name}`, () => {
     it('should throw an error', () => {
@@ -9,5 +9,16 @@ describe(`Test fn ${abstractFn.name}`, () => {
 describe(`Test fn ${abstractAsyncFn.name}`, () => {
     it('should throw an error', async () => {
         await expect((): Promise<never> => abstractAsyncFn()).rejects.toThrow('Not Implemented');
+    });
+});
+
+describe(`Test fn ${noAsync.name}`, () => {
+    it('should drop the returned Promise', async () => {
+        function fn() {
+            return Promise.resolve();
+        }
+
+        await expect(fn()).resolves.toBeUndefined();
+        expect(noAsync(fn)()).toBeUndefined();
     });
 });
--- packages/utils/src/function.ts
+++ packages/utils/src/function.ts
@@ -5,3 +5,15 @@ export function abstractFn(): never {
 export function abstractAsyncFn(): Promise<never> {
     return Promise.reject(new Error('Not Implemented'));
 }
+
+/**
+ * Convert an async function to a sync function by dropping the returned Promise.
+ *
+ * This is useful for bypassing ESLing rule `@typescript-eslint/no-misused-promises` in some cases
+ * when we want to pass an async function to a receiver that expects a functionwith return type `void`.
+ *
+ * **WARNING**: Please ensure it's safe to drop the returned Promise if you want to use this function.
+ */
+export function noAsync<T extends (...args: never[]) => unknown>(fn: T): (...args: Parameters<T>) => void {
+    return (...args: Parameters<T>) => void fn(...args);
+}
