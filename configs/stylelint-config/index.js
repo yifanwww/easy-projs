4de6b302c415990d53bded76153d384e279557a4
yifanwww <yifanw1101@gmail.com>
2023-09-06 01:15:13 +0800

    chore(deps): refresh deps (#43)
    * chore(deps): refresh deps
    
    * refactor: format code

--- configs/stylelint-config/index.js
+++ configs/stylelint-config/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     extends: ['stylelint-config-sass-guidelines', 'stylelint-config-recess-order'],
     rules: {
-        indentation: 4,
         // https://stylelint.io/user-guide/rules/max-nesting-depth/
         'max-nesting-depth': null,
         'selector-class-pattern': null,
