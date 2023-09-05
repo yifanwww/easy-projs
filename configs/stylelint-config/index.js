5189abc83b9ec355fffd10c7bbafec1dde2965db
yifanwww <yifanw1101@gmail.com>
2023-09-06 01:33:49 +0800

    chore(stylelint): add links for each rule

--- configs/stylelint-config/index.js
+++ configs/stylelint-config/index.js
@@ -3,9 +3,14 @@ module.exports = {
     rules: {
         // https://stylelint.io/user-guide/rules/max-nesting-depth/
         'max-nesting-depth': null,
+
+        // https://stylelint.io/user-guide/rules/selector-class-pattern/
         'selector-class-pattern': null,
+
+        // https://stylelint.io/user-guide/rules/selector-max-id/
         'selector-max-id': null,
 
+        // https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-alphabetical-order/README.md
         'order/properties-alphabetical-order': null,
     },
 };
