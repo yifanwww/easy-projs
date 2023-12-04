6686013f0fe1a3f6a7a38a3589ce2f2e36314e76
yifanwww <yifanw1101@gmail.com>
2023-12-05 00:13:31 +0800

    feat(utils-test): add env mocker

--- packages/utils-test/src/index.ts
+++ packages/utils-test/src/index.ts
@@ -1,4 +1,5 @@
 export * from './class';
 export * from './console';
+export * from './mockEnv';
 export * from './ReactHook';
 export * from './storage';
---
+++ packages/utils-test/src/mockEnv.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+export function createProcessNodeEnvMocker() {
+    const INITIAL_PROCESS_ENV = process.env;
+
+    function mockProcessNodeEnv(value: typeof process.env.NODE_ENV) {
+        process.env = {
+            ...INITIAL_PROCESS_ENV,
+            NODE_ENV: value,
+        };
+    }
+
+    function resetProcessNodeEnv() {
+        process.env = { ...INITIAL_PROCESS_ENV };
+    }
+
+    return { mockProcessNodeEnv, resetProcessNodeEnv };
+}
+
+export function createWindowLocationMocker() {
+    const INITIAL_WINDOW_LOCATION = window.location;
+
+    function mockWindowLocation(url: string) {
+        // @ts-ignore
+        delete window.location;
+        // @ts-ignore
+        window.location = new URL(url);
+    }
+
+    function resetWindowLocation() {
+        // @ts-ignore
+        delete window.location;
+        window.location = INITIAL_WINDOW_LOCATION;
+    }
+
+    return { mockWindowLocation, resetWindowLocation };
+}
