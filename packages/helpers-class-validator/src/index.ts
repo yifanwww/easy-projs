1cdad41c5e5e0e9b56f8ab9428e8494094a5c35d
yifanwww <yifanw1101@gmail.com>
2024-02-15 23:05:31 +0800

    feat(pkg/helpers-class-validator): add IsEmptyStringIgnorable

--- packages/helpers-class-validator/src/index.ts
+++ packages/helpers-class-validator/src/index.ts
@@ -1,10 +1,14 @@
 import type { ValidationOptions } from 'class-validator';
 import { ValidateIf } from 'class-validator';
 
-export function isNullable(options?: ValidationOptions): PropertyDecorator {
+export function IsNullable(options?: ValidationOptions): PropertyDecorator {
     return ValidateIf((obj, value) => value !== null, options);
 }
 
-export function isUndefinable(options?: ValidationOptions): PropertyDecorator {
+export function IsUndefinable(options?: ValidationOptions): PropertyDecorator {
     return ValidateIf((obj, value) => value !== undefined, options);
+}
+
+export function IsEmptyStringIgnorable(options?: ValidationOptions): PropertyDecorator {
+    return ValidateIf((obj, value) => value !== '', options);
 }
