27d92bce216191005e4ee1225f14615091017b34
yifanwww <yifanw1101@gmail.com>
2024-02-20 14:32:40 +0800

    chore(proj/webapp): disable vite-plugin-checker option `enableBuild`

--- projects/webapp/vite.config.ts
+++ projects/webapp/vite.config.ts
@@ -7,8 +7,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import packageJson from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-    plugins: [react(), tsconfigPaths(), command === 'serve' && checker({ typescript: true })],
+export default defineConfig(() => ({
+    plugins: [react(), tsconfigPaths(), checker({ enableBuild: false, typescript: true })],
     define: {
         __APP_VERSION__: JSON.stringify(packageJson.version),
         __APP_HASH__: JSON.stringify(child.execSync('git rev-parse HEAD').toString().trim()),
