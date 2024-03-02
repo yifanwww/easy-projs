d9d443960fe95a24e90c8704a61cdd15b42ef166
yifanwww <yifanw1101@gmail.com>
2024-03-03 01:14:18 +0800

    feat(pkg/utils-node): add assertIsDefined

--- packages/utils-node/src/__tests__/assert.test.ts
+++ packages/utils-node/src/__tests__/assert.test.ts
@@ -1,11 +1,20 @@
 import { describe, expect, it } from '@jest/globals';
 import { AssertionError } from 'node:assert';
 
-import { assertIsNever } from '../assert.js';
+import { assertIsDefined, assertIsNever } from '../assert.js';
 
 describe(`Test fn \`${assertIsNever.name}\``, () => {
     it('should throw an error', () => {
         expect(() => assertIsNever('hello world' as never)).toThrow(AssertionError);
         expect(() => assertIsNever('hello world' as never)).toThrowErrorMatchingSnapshot();
+    });
+});
+
+describe(`Test fn \`${assertIsDefined.name}\``, () => {
+    it('should throw an error if assertion failes', () => {
+        expect(() => assertIsDefined(null)).toThrow(AssertionError);
+        expect(() => assertIsDefined(undefined)).toThrow(AssertionError);
+        expect(() => assertIsDefined(null)).toThrowErrorMatchingSnapshot();
+        expect(() => assertIsDefined(undefined)).toThrowErrorMatchingSnapshot();
     });
 });
--- packages/utils-node/src/assert.ts
+++ packages/utils-node/src/assert.ts
@@ -4,5 +4,20 @@ import { AssertionError } from 'node:assert';
  * The function for asserting whether a value's type is `never`.
  */
 export function assertIsNever(value: never): never {
-    throw new AssertionError({ message: `"${String(value)}" should be "never" type.` });
+    throw new AssertionError({
+        message: `"${String(value)}" should be "never" type.`,
+        actual: value,
+    });
+}
+
+/**
+ * The function for asserting whether a value is defined (!== `undefined` && !== `null`).
+ */
+export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
+    if (value === undefined || value === null) {
+        throw new AssertionError({
+            message: `Expected 'value' to be defined, but received \`${value as null | undefined}\``,
+            actual: value,
+        });
+    }
 }
