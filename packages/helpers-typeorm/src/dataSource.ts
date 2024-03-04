b81c7171befed77a44e83edf4b6f17847a6216a8
yifanwww <yifanw1101@gmail.com>
2024-03-05 01:49:46 +0800

    feat(pkg/helpers-typeorm): add DataSourceHelper

--- packages/helpers-typeorm/src/dataSource.ts
+++ packages/helpers-typeorm/src/dataSource.ts
@@ -1,27 +1,28 @@
 import { Err, type Result } from 'rustlike-result';
 import { type EntityManager, type DataSource } from 'typeorm';
 
-export async function resultifiedTransaction<Res, Err>(
-    dataSource: DataSource,
-    runInTransaction: (entityManager: EntityManager) => Promise<Result<Res, Err>>,
-): Promise<Result<Res, Err | Error>> {
-    const queryRunner = dataSource.createQueryRunner();
+export class DataSourceHelper {
+    static async transaction<Res, Err>(
+        dataSource: DataSource,
+        runInTransaction: (entityManager: EntityManager) => Promise<Result<Res, Err>>,
+    ): Promise<Result<Res, Err | Error>> {
+        const queryRunner = dataSource.createQueryRunner();
 
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-    try {
-        const result = await runInTransaction(queryRunner.manager);
-        if (result.isErr()) {
-            await queryRunner.rollbackTransaction();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const result = await runInTransaction(queryRunner.manager);
+            if (result.isOk()) {
+                await queryRunner.commitTransaction();
+            } else {
+                await queryRunner.rollbackTransaction();
+            }
             return result;
+        } catch (err) {
+            await queryRunner.rollbackTransaction();
+            return err instanceof Error ? Err(err) : Err(new Error(typeof err === 'string' ? err : 'unknown error'));
+        } finally {
+            await queryRunner.release();
         }
-
-        await queryRunner.commitTransaction();
-        return result;
-    } catch (err) {
-        await queryRunner.rollbackTransaction();
-        return err instanceof Error ? Err(err) : Err(new Error(typeof err === 'string' ? err : 'unknown error'));
-    } finally {
-        await queryRunner.release();
     }
 }
