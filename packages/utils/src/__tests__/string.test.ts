fadb06584b8a34ff101b08f50c9f3f9742c54966
yifanwww <yifanw1101@gmail.com>
2024-03-08 23:22:25 +0800

    feat(pkg/utils): add StringUtil.startsWithOneOf

--- packages/utils/src/__tests__/string.test.ts
+++ packages/utils/src/__tests__/string.test.ts
@@ -4,16 +4,27 @@ import { StringUtil } from '../string.js';
 
 describe(`Test static method \`${StringUtil.name}.${StringUtil.isNilOrEmpty.name}\``, () => {
     it('should return true when checks null or undefined', () => {
-        expect(StringUtil.isNilOrEmpty(null)).toBeTruthy();
-        expect(StringUtil.isNilOrEmpty(undefined)).toBeTruthy();
+        expect(StringUtil.isNilOrEmpty(null)).toBe(true);
+        expect(StringUtil.isNilOrEmpty(undefined)).toBe(true);
     });
 
     it('should return true when checks an empty string', () => {
-        expect(StringUtil.isNilOrEmpty('')).toBeTruthy();
+        expect(StringUtil.isNilOrEmpty('')).toBe(true);
     });
 
     it('should return false when checks a string which is not empty', () => {
-        expect(StringUtil.isNilOrEmpty('a')).toBeFalsy();
-        expect(StringUtil.isNilOrEmpty('abcd')).toBeFalsy();
+        expect(StringUtil.isNilOrEmpty('a')).toBe(false);
+        expect(StringUtil.isNilOrEmpty('abcd')).toBe(false);
+    });
+});
+
+describe(`Test static method \`${StringUtil.name}.${StringUtil.startsWithOneOf.name}\``, () => {
+    it('should check if a value starts with one of search strings', () => {
+        expect(StringUtil.startsWithOneOf('hello world', [])).toBe(false);
+        expect(StringUtil.startsWithOneOf('hello world', ['hello'])).toBe(true);
+        expect(StringUtil.startsWithOneOf('hello world', ['world'])).toBe(false);
+        expect(StringUtil.startsWithOneOf('hello world', ['hello', 'world'])).toBe(true);
+        expect(StringUtil.startsWithOneOf('hello world', ['hello', 'world'], 2)).toBe(false);
+        expect(StringUtil.startsWithOneOf('hello world', ['hello', 'world', 'llo'], 2)).toBe(true);
     });
 });
--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -2,4 +2,8 @@ export class StringUtil {
     static isNilOrEmpty(value: unknown): value is undefined | null | '' {
         return value === undefined || value === null || value === '';
     }
+
+    static startsWithOneOf(str: string, searchStrings: readonly string[], position?: number) {
+        return searchStrings.some((item) => str.startsWith(item, position));
+    }
 }
