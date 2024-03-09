eeb6bc1f1967a0dd2cbb30c3c068892a0f003211
yifanwww <yifanw1101@gmail.com>
2024-03-09 19:35:51 +0800

    test(pkg/helpers-nestjs): simplify unit tests

--- packages/helpers-nestjs/src/dto/__tests__/pagination.dto.test.ts
+++ packages/helpers-nestjs/src/dto/__tests__/pagination.dto.test.ts
@@ -22,125 +22,77 @@ describe(`Test dto class \`${PaginationQueryDto.name}}\``, () => {
         }
     });
 
-    it('should not pass validation when request is invalid [page="bad value"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page: 'bad value',
+    function testFields(field: 'page' | 'page_size') {
+        it(`should not pass validation when request is invalid [${field}="bad value"]`, async () => {
+            const model = plainToInstance(PaginationQueryDto, {
+                page: '1',
+                page_size: '10',
+                [field]: 'bad value',
+            });
+
+            const errors = await new Validator().validate(model);
+            expect(errors).toHaveLength(1);
+            expect(errors[0].property).toBe(field);
+            expect(errors[0].constraints).toStrictEqual({
+                isInt: `${field} must be an integer number`,
+                isPositive: `${field} must be a positive number`,
+            });
+            expect(errors[0].value).toBeNaN();
+            expect(errors[0].children).toHaveLength(0);
         });
 
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page must be an integer number',
-            isPositive: 'page must be a positive number',
-        });
-        expect(errors[0].value).toBeNaN();
-        expect(errors[0].children).toHaveLength(0);
-    });
+        it(`should not pass validation when request is invalid [${field}="Infinity"]`, async () => {
+            const model = plainToInstance(PaginationQueryDto, {
+                page: '1',
+                page_size: '10',
+                [field]: 'Infinity',
+            });
 
-    it('should not pass validation when request is invalid [page="Infinity"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page: 'Infinity',
-        });
-
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page must be an integer number',
-        });
-        expect(errors[0].value).toBe(Infinity);
-        expect(errors[0].children).toHaveLength(0);
-    });
-
-    it('should not pass validation when request is invalid [page="1.2"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page: '1.2',
+            const errors = await new Validator().validate(model);
+            expect(errors).toHaveLength(1);
+            expect(errors[0].property).toBe(field);
+            expect(errors[0].constraints).toStrictEqual({
+                isInt: `${field} must be an integer number`,
+            });
+            expect(errors[0].value).toBe(Infinity);
+            expect(errors[0].children).toHaveLength(0);
         });
 
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page must be an integer number',
-        });
-        expect(errors[0].value).toBe(1.2);
-        expect(errors[0].children).toHaveLength(0);
-    });
+        it(`should not pass validation when request is invalid [${field}="1.2"]`, async () => {
+            const model = plainToInstance(PaginationQueryDto, {
+                page: '1',
+                page_size: '10',
+                [field]: '1.2',
+            });
 
-    it('should not pass validation when request is invalid [page="0"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page: '0',
-        });
-
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page');
-        expect(errors[0].constraints).toStrictEqual({
-            isPositive: 'page must be a positive number',
-        });
-        expect(errors[0].value).toBe(0);
-        expect(errors[0].children).toHaveLength(0);
-    });
-
-    it('should not pass validation when request is invalid [page_size="bad value"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page_size: 'bad value',
+            const errors = await new Validator().validate(model);
+            expect(errors).toHaveLength(1);
+            expect(errors[0].property).toBe(field);
+            expect(errors[0].constraints).toStrictEqual({
+                isInt: `${field} must be an integer number`,
+            });
+            expect(errors[0].value).toBe(1.2);
+            expect(errors[0].children).toHaveLength(0);
         });
 
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page_size');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page_size must be an integer number',
-            isPositive: 'page_size must be a positive number',
-        });
-        expect(errors[0].value).toBeNaN();
-        expect(errors[0].children).toHaveLength(0);
-    });
+        it(`should not pass validation when request is invalid [${field}="0"]`, async () => {
+            const model = plainToInstance(PaginationQueryDto, {
+                page: '1',
+                page_size: '10',
+                [field]: '0',
+            });
 
-    it('should not pass validation when request is invalid [page_size="Infinity"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page_size: 'Infinity',
+            const errors = await new Validator().validate(model);
+            expect(errors).toHaveLength(1);
+            expect(errors[0].property).toBe(field);
+            expect(errors[0].constraints).toStrictEqual({
+                isPositive: `${field} must be a positive number`,
+            });
+            expect(errors[0].value).toBe(0);
+            expect(errors[0].children).toHaveLength(0);
         });
+    }
 
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page_size');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page_size must be an integer number',
-        });
-        expect(errors[0].value).toBe(Infinity);
-        expect(errors[0].children).toHaveLength(0);
-    });
-
-    it('should not pass validation when request is invalid [page_size="1.2"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page_size: '1.2',
-        });
-
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page_size');
-        expect(errors[0].constraints).toStrictEqual({
-            isInt: 'page_size must be an integer number',
-        });
-        expect(errors[0].value).toBe(1.2);
-        expect(errors[0].children).toHaveLength(0);
-    });
-
-    it('should not pass validation when request is invalid [page_size="0"]', async () => {
-        const model = plainToInstance(PaginationQueryDto, {
-            page_size: '0',
-        });
-
-        const errors = await new Validator().validate(model);
-        expect(errors).toHaveLength(1);
-        expect(errors[0].property).toBe('page_size');
-        expect(errors[0].constraints).toStrictEqual({
-            isPositive: 'page_size must be a positive number',
-        });
-        expect(errors[0].value).toBe(0);
-        expect(errors[0].children).toHaveLength(0);
-    });
+    testFields('page');
+    testFields('page_size');
 });
