2076d6d0744302e859ecc5f2470061828a0f6e8e
yifanwww <yifanw1101@gmail.com>
2024-09-29 23:34:36 +0800

    feat(helpers/nestjs): add ExperimentalApiGuard

---
+++ packages/helpers-nestjs/src/guards/ExperimentalApiGuard.ts
@@ -0,0 +1,21 @@
+import { type CanActivate, type ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+const EXPERIMENTAL_API = 'EXPERIMENTAL_API';
+
+@Injectable()
+export class ExperimentalApiGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const experimentalApi = this.reflector.getAllAndOverride<boolean | undefined>(EXPERIMENTAL_API, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        return !experimentalApi || process.env.EXPERIMENTAL === 'true';
+    }
+}
+
+export function ExperimentalApi() {
+    return SetMetadata(EXPERIMENTAL_API, true);
+}
--- packages/helpers-nestjs/src/index.ts
+++ packages/helpers-nestjs/src/index.ts
@@ -5,4 +5,5 @@ export * from './auth/password.js';
 export type * from './auth/types/jwt.js';
 export * from './dto/ApiPagination.dto.js';
 export * from './dto/param.dto.js';
+export * from './guards/ExperimentalApiGuard.js';
 export * from './logger/index.js';
