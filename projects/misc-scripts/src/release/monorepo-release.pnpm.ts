719bcd27b03548e9234c54677c957bdb8fa8b2ba
yifanwww <yifanw1101@gmail.com>
2024-11-10 01:43:24 +0800

    feat(misc-scripts): add release script for pnpm monorepo

---
+++ projects/misc-scripts/src/release/monorepo-release.pnpm.ts
@@ -0,0 +1,51 @@
+import type { Nullable } from '@easy-pkg/types';
+import child from 'node:child_process';
+import semver from 'semver';
+
+function getNpmTag(prerelease: Nullable<readonly (string | number)[]>): string {
+    const type = typeof prerelease?.[0] !== 'string' ? null : prerelease[0];
+
+    switch (type) {
+        case null:
+            return 'latest';
+
+        case 'alpha':
+        case 'beta':
+        case 'next':
+        case 'rc':
+            return type;
+
+        default:
+            return 'next';
+    }
+}
+
+const ALLOWED_PACKAGE_NAMES = ['<your_package_name>'];
+
+function release(): void {
+    // This environment variable is injected in Github Actions
+    //
+    // ```yml
+    // - name: Publish
+    //   run: node scripts/release.js
+    //   env:
+    //     GITHUB_REF_NAME: ${{ github.ref_name }}
+    // ```
+    const tag = process.env.GITHUB_REF_NAME;
+    if (!tag) return;
+
+    // The tag name format is: `{package_name}_{version}`
+    // for example: @scope/name_v0.0.0, @scope/name_v0.0.0-alpha.1
+    const [packageName, version] = tag.split('_');
+
+    if (!ALLOWED_PACKAGE_NAMES.includes(packageName)) return;
+
+    const prerelease = semver.prerelease(version);
+    const npmTag = getNpmTag(prerelease);
+
+    const cmd = `pnpm publish --filter ${packageName} --tag ${npmTag} --no-git-checks`;
+    console.info(cmd);
+    child.execSync(cmd, { encoding: 'utf-8', stdio: 'inherit' });
+}
+
+release();
---
+++ projects/misc-scripts/src/release/release.pnpm.ts
@@ -0,0 +1,43 @@
+import type { Nullable } from '@easy-pkg/types';
+import child from 'node:child_process';
+import semver from 'semver';
+
+function getNpmTag(prerelease: Nullable<readonly (string | number)[]>): string {
+    const type = typeof prerelease?.[0] !== 'string' ? null : prerelease[0];
+
+    switch (type) {
+        case null:
+            return 'latest';
+
+        case 'alpha':
+        case 'beta':
+        case 'next':
+        case 'rc':
+            return type;
+
+        default:
+            return 'next';
+    }
+}
+
+function release(): void {
+    // This environment variable is injected in Github Actions
+    //
+    // ```yml
+    // - name: Publish
+    //   run: node scripts/release.js
+    //   env:
+    //     GITHUB_REF_NAME: ${{ github.ref_name }}
+    // ```
+    const version = process.env.GITHUB_REF_NAME;
+    if (!version) return;
+
+    const prerelease = semver.prerelease(version);
+    const npmTag = getNpmTag(prerelease);
+
+    const cmd = `pnpm publish --tag ${npmTag} --no-git-checks`;
+    console.info(cmd);
+    child.execSync(cmd, { encoding: 'utf-8', stdio: 'inherit' });
+}
+
+release();
