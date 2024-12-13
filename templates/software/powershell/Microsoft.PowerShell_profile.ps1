b998e80e8a5991a86258f695fc3b1f2819034116
yifanwww <yifanw1101@gmail.com>
2024-12-13 23:14:36 +0800

    feat(template/software): set powershell console encoding to utf8

--- templates/software/powershell/Microsoft.PowerShell_profile.ps1
+++ templates/software/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
 # $profile.CurrentUserCurrentHost   %UserProfile%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 # $profile.AllUsersCurrentHost      C:\Program Files\PowerShell\7\Microsoft.PowerShell_profile.ps1
 
+Import-Module posh-git
+
 oh-my-posh init pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/yifanwww.omp.json | Invoke-Expression
 
 fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --shell power-shell | Out-String | Invoke-Expression
+
+[Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
